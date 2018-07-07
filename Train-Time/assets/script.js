971cd94ac61b5ed03de45c32c71b6a57058d27cc
kr0sette <37877021+kr0sette@users.noreply.github.com>
2018-07-07 09:46:07 -0400

    Adding Train Time activity

---
+++ Train-Time/assets/script.js
@@ -0,0 +1,33 @@
+//Instructs computer to read HTML before running the javascript
+$(document).ready(function(){
+
+
+//Initialize firebase
+//Create variable to reference database
+//initial values
+
+//Button on click event
+    //event.preventDefault();
+    //Store form input in to variables
+    //Push input back to firebase
+
+//Firebase watcher + initial loader
+    //Log what is coming out of snapshot
+    //Add item as a row in a table???
+    //handle errors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
